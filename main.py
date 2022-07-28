f2b19522e9cef1dd89cb0ef027bde0f5aaeaf73a
squirrels1 <110204482+squirrels1@users.noreply.github.com>
2022-07-28 15:29:55 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,44 @@
+import os
+import discord
+import aiohttp
+from colorama import Fore
+from pkgutil import iter_modules
+from discord.ext import commands, tasks
+from keep_alive import keep_alive
+
+class MyBot(commands.Bot):
+    def __init__(self):
+      super().__init__(
+          command_prefix = '-',
+          intents=discord.Intents.all(),
+          activity=discord.Activity(type=discord.ActivityType.listening,name="-help"),
+          owner_ids=[706242014056022027]
+      )
+      self.initial_extensions = [m.name for m in iter_modules(['cogs'], prefix='cogs.')]; self.initial_extensions.append("jishaku")
+
+    async def setup_hook(self):
+      self.background_task.start()
+      self.session = aiohttp.ClientSession()
+      for ext in self.initial_extensions:
+        try:
+          await self.load_extension(ext)
+          print(f"{Fore.GREEN} {ext} loaded {Fore.RESET}")
+        except:
+          print(f"{Fore.RED} {ext} not loaded {Fore.RESET}")
+        
+    @tasks.loop(minutes=10)
+    async def background_task(self):
+        print('Running background task...')
+  
+    async def close(self):
+      await super().close()
+      await self.session.close()
+
+    async def on_ready(self):
+      print(Fore.MAGENTA + "========[ " + bot.user.name + " is Online! ]========" + Fore.RESET)
+      print(Fore.RED + "========[ Discord Version: " + discord.__version__ + " ]=======")
+
+keep_alive()
+bot = MyBot()
+my_secret = os.environ['TOKEN']
+bot.run(my_secret)
